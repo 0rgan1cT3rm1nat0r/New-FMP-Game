03cbc22ebf033c8acb9bed0acc1ee13e0cc65c9d
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2025-04-30 11:49:49 +0100

    wworrd

--- FMProject/Assets/Scripts/ShooterDirection.cs
+++ FMProject/Assets/Scripts/ShooterDirection.cs
@@ -1,76 +1,42 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShooterDirection : MonoBehaviour
 {
-    // Speed at which the aim rotates, tweak this value for faster/slower rotation
     public float rotationSpeed = 10000f;
+
+    private float lastHorizontalDirection = 1f; // Start by facing right
 
     void Update()
     {
-        // Get horizontal (A/D or Left/Right arrow) and vertical (W/S or Up/Down arrow) input
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        // Create a vector to represent the direction of the aim based on input
-        Vector3 direction = new Vector3( -vertical, horizontal, 0f);
+        // Update last horizontal direction if moving
+        if (Mathf.Abs(horizontal) > 0.1f)
+        {
+            lastHorizontalDirection = Mathf.Sign(horizontal); // +1 = right, -1 = left
+        }
 
-        // Only rotate if there is input
+        Vector3 direction;
+
+        // If there is input in any direction, use it
+        if (Mathf.Abs(vertical) > 0.1f || Mathf.Abs(horizontal) > 0.1f)
+        {
+            direction = new Vector3(-vertical, horizontal, 0f);
+        }
+        else
+        {
+            // No input, default to last known horizontal direction
+            direction = (lastHorizontalDirection > 0) ? Vector3.up : Vector3.down;
+        }
+
+        // Rotate if we have a valid direction
         if (direction.magnitude > 0.1f)
         {
-            // Calculate the target rotation based on the input direction
             Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
-
-            // Smoothly rotate towards the target rotation
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
     }
 }
-
-
-
-/*
- using UnityEngine;
-
-public class RotateAimWithKeyPress : MonoBehaviour
-{
-    // Speed at which the aim rotates
-    public float rotationSpeed = 10f;
-
-    void Update()
-    {
-        // Create a vector to store movement input
-        Vector3 direction = Vector3.zero;
-
-        // Check if specific keys are being held down
-        if (Input.GetKey(KeyCode.W)) // Up
-        {
-            direction += Vector3.up;
-        }
-        if (Input.GetKey(KeyCode.S)) // Down
-        {
-            direction += Vector3.down;
-        }
-        if (Input.GetKey(KeyCode.A)) // Left
-        {
-            direction += Vector3.left;
-        }
-        if (Input.GetKey(KeyCode.D)) // Right
-        {
-            direction += Vector3.right;
-        }
-
-        // Only rotate if there is input
-        if (direction.magnitude > 0.1f)
-        {
-            // Calculate the target rotation based on the input direction
-            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
-
-            // Smoothly rotate towards the target rotation
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        }
-    }
-}
-*/
