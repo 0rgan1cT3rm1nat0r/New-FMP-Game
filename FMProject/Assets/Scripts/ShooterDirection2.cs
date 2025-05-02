4512384c67f8a6e3db19fb19842cda3255ccccef
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2025-05-02 11:53:53 +0100

    PROGRESS AAAAA

---
+++ FMProject/Assets/Scripts/ShooterDirection2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterDirection2 : MonoBehaviour
+{
+    public float rotationSpeed = 10000f;
+
+    private float lastHorizontalDirection = 1f; // Start by facing right
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal2");
+        float vertical = Input.GetAxis("Vertical2");
+
+        // Update last horizontal direction if moving
+        if (Mathf.Abs(horizontal) > 0.1f)
+        {
+            lastHorizontalDirection = Mathf.Sign(horizontal); // +1 = right, -1 = left
+        }
+
+        Vector3 direction;
+
+        // If there is input in any direction, use it
+        if (Mathf.Abs(vertical) > 0.1f || Mathf.Abs(horizontal) > 0.1f)
+        {
+            direction = new Vector3(-vertical, horizontal, 0f);
+        }
+        else
+        {
+            // No input, default to last known horizontal direction
+            direction = (lastHorizontalDirection > 0) ? Vector3.up : Vector3.down;
+        }
+
+        // Rotate if we have a valid direction
+        if (direction.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
