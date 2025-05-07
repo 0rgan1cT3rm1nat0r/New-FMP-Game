b01c2670970749cc29d95f372de1b26922ea9253
0rgan1cT3rm1nat0r <rmarshall2023@Elam.co.uk>
2025-05-07 11:54:13 +0100

    vclk
    hnjcdgj

--- FMProject/Assets/Scripts/EnemyAI.cs
+++ FMProject/Assets/Scripts/EnemyAI.cs
@@ -45,10 +45,11 @@ public class EnemyAI : MonoBehaviour
                 agent.isStopped = true;
 
                 // Optional: rotate to face target
+                /*
                 Vector3 direction = (targetPlayer.transform.position - transform.position).normalized;
                 Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
                 transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
-
+                */
                 TryAttack();
             }
             else
